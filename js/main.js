558a47c01552384e4c5f8ff1946db263357b6d7a
Tim McKenna - YCP <47125817+YCPTimMcKenna@users.noreply.github.com>
2020-11-24 07:53:03 -0800

    TIM EDITS/Mobile Styling
    What I did:
    - Cleaned up iphone styling a touch to make sure the off canvas nav appears
    - Cleaned up a some styling for the tour about about pages
    - Tried creating a new gif...though I think we will need to this in Photoshop
    
    What you'll do next:
    - Review styling, finishing touches...then complete!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("main").style.marginRight = "350px";
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginRight = "200px";
   }
   
   function closeNav() {
